91fbfe67debf9003060ebc3f89640f01904349a1
Joseph Wang <joseph4x4@gmail.com>
2024-08-07 02:11:41 -0400

    Updated optimizer

--- Top Drives/campaign_optimizer.py
+++ Top Drives/campaign_optimizer.py
@@ -1,8 +1,43 @@
 from collections import Counter
-from json import load
+from json import dump, load
+from random import choice
 
 
-def test_reduction():
+def score(solution):
+    return len(solution) + sum([len(solution[key]) for key in solution])
+
+
+def print_results(campaign):
+    max_len_name = max([len(loc) for loc in campaign])
+    output = []
+    for loc, match in campaign.items():
+        if loc == "UK Midlands 7":
+            output.append(f"{loc:<{max_len_name}}: 1")
+            output.append(f"{loc:<{max_len_name}}:  2")
+            continue
+        race_nums = [
+            str(idx+1) if match[idx] else ' '
+            for idx in range(len(match))
+        ]
+        output.append(
+            f"{loc:<{max_len_name}}: "
+            f"{''.join(race_nums)}"
+        )
+    
+    # for line in output:
+    #     print(line)
+    return output
+
+
+def reorder_solution(campaign, solution):
+    new_solution = {}
+    for loc in campaign:
+        if loc in solution:
+            new_solution[loc] = solution[loc]
+    return new_solution
+
+
+def read_campaign():
     with open("campaign.json") as f:
         campaign = [
             d for d in load(f)
@@ -43,6 +78,20 @@ def test_reduction():
                     nblockers -= 1
                 pos -= 1
     
+    with open("campaign_reqs_removed.json", "w") as f:
+        dump(campaign, f)
+
+
+def reduction_attempt_1():
+    with open("campaign_reqs_removed.json") as f:
+        campaign = load(f)
+    
+    race_counter = Counter([
+        race
+        for loc, match in campaign.items()
+        for race in match
+    ])
+    
     # Remove entire matches where possible
     for loc, match in campaign.items():
         counts = [race_counter[race] > 1 for race in match]
@@ -63,29 +112,133 @@ def test_reduction():
     
     campaign = {k: v for k, v in campaign.items() if v and any(v)}
 
-    max_len_name = max([len(loc) for loc in campaign])
-
-    output = []
-    for loc, match in campaign.items():
-        if loc == "UK Midlands 7":
-            output.append(f"{loc:<{max_len_name}}: 1")
-            output.append(f"{loc:<{max_len_name}}:  2")
-            continue
-        race_nums = [
-            str(idx+1) if match[idx] else ' '
-            for idx in range(len(match))
-        ]
-        output.append(
-            f"{loc:<{max_len_name}}: "
-            f"{''.join(race_nums)}"
-        )
+    output = print_results(campaign)
     
-    for line in output:
-        print(line)
+    optimized_score = score(campaign)
+    print(optimized_score)
     
-    with open("output.txt", "w") as f:
-        f.write("\n".join(output))
+    if optimized_score < 240:
+        with open(f"output_{optimized_score}.txt", "w") as f:
+            f.write("\n".join(output))
 
 
+def reduction_attempt_2():
+    with open("campaign_reqs_removed.json") as f:
+        campaign = load(f)
+    
+    race_counter = Counter([
+        race
+        for loc, match in campaign.items()
+        for race in match
+    ])
 
-test_reduction()
+    covered_tracks = set()
+    solution = {}
+    while len(covered_tracks) < len(race_counter):
+        candidates = []
+        for loc, match in campaign.items():
+            existing_length = 0 if loc not in solution else len(solution[loc])
+            for length in range(existing_length + 1, len(match) + 1):
+                submatch = match[:length]
+                new_tracks = set(submatch) - covered_tracks
+                cost = length - existing_length + (0 if loc in solution else 1)
+                # Ensure unique tracks get added asap
+                if race_counter[submatch[-1]] == 1:
+                    cost = 0
+                if new_tracks:
+                    candidates.append((loc, submatch, new_tracks, cost))
+        
+        candidates.sort(key=lambda x: (x[3], -len(new_tracks), len(x[1])))
+
+        best_loc, best_submatch, new_tracks, _ = candidates[0]
+        solution[best_loc] = best_submatch
+        covered_tracks.update(new_tracks)
+        print(f"{len(covered_tracks)}/{len(race_counter)}")
+    
+    optimized_score = score(solution)
+    print(optimized_score)
+
+
+def reduction_attempt_3():
+    with open("campaign_reqs_removed.json") as f:
+        campaign = load(f)
+    
+    race_counter = Counter([
+        race
+        for loc, match in campaign.items()
+        for race in match
+    ])
+
+    covered_tracks = set()
+    solution = {}
+    while len(covered_tracks) < len(race_counter):
+        candidates = []
+        for loc, match in campaign.items():
+            existing_length = 0 if loc not in solution else len(solution[loc])
+            for length in range(existing_length + 1, len(match) + 1):
+                submatch = match[:length]
+                new_tracks = set(submatch) - covered_tracks
+                cost = length - existing_length + (0 if loc in solution else 1)
+                # Ensure unique tracks get added asap
+                if race_counter[submatch[-1]] == 1:
+                    cost = 0
+                if new_tracks:
+                    candidates.append((loc, submatch, new_tracks, cost))
+        
+        candidates.sort(key=lambda x: x[3])
+        min_cost = [tup for tup in candidates if tup[3] == candidates[0][3]]
+
+        best_loc, best_submatch, new_tracks, _ = choice(min_cost)
+        solution[best_loc] = best_submatch
+        covered_tracks.update(new_tracks)
+    
+    solution = reorder_solution(campaign, solution)
+    optimized_score = score(solution)
+    output = print_results(solution)
+    print(optimized_score)
+    
+    if optimized_score < 230:
+        with open(f"output_{optimized_score}.txt", "w") as f:
+            f.write("\n".join(output))
+    
+    return optimized_score
+
+
+def create_link(score):
+    with open(f"output_{score}.txt") as f:
+        output = f.read()
+    with open("campaign_reqs_removed.json") as f:
+        campaign = load(f)
+    linktext = []
+    base = "https://www.topdrivesrecords.com?share="
+    delim = "~K"
+    limit = 10
+    cur_tracks = []
+    cur_locs = []
+    for line in output.split("\n"):
+        loc, nums = line.split(": ")
+        loc = loc.strip()
+        nums = [int(d) - 1 for d in nums if d.isdigit()]
+        selected = [campaign[loc][i] for i in nums]
+        if len(cur_tracks) + len(selected) > limit:
+            link = base + delim + delim.join(cur_tracks)
+            linktext.extend([link] + cur_locs + [" "])
+            cur_tracks = []
+            cur_locs = []
+        cur_tracks.extend(selected)
+        cur_locs.append(line)
+    if cur_tracks:
+        link = base + delim + delim.join(cur_tracks)
+        linktext.extend([link] + cur_locs)
+    with open("TDR_links.txt", "w") as f:
+        f.write("\n".join(linktext))
+
+
+# read_campaign()
+# reduction_attempt_1()
+# reduction_attempt_2()
+# for _ in range(10000):
+#     optimized_score = reduction_attempt_3()
+#     if optimized_score < 230:
+#         break
+# create_link(230)
